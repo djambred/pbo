2e83e9b333a39130a99f4b0cb976fbe92a00556f
djambred <djambred@gmail.com>
2021-10-21 20:38:21 +0700

    pert 5 segitiga

---
+++ pert5/segitiga.cc
@@ -0,0 +1,40 @@
+#include<iostream>
+
+using namespace std;
+
+class Segitiga{
+    private:
+        int alas, tinggi;
+    public:
+        int getAlas(){
+            return alas;
+        }
+        int getTinggi(){
+            return tinggi;
+        }
+
+        void setAlas(int a){
+            alas = a;
+        }
+
+        void setTinggi(int t){
+            tinggi = t;
+        }
+
+        int hitungLuas(){
+            return 0.5*alas*tinggi;
+        }
+
+};
+
+int main(){
+    Segitiga LuasSegitiga;
+
+    LuasSegitiga.setAlas(2);
+    LuasSegitiga.setTinggi(5);
+
+
+    cout << "Luas : "<<LuasSegitiga.hitungLuas()<<endl;
+
+    return 0;
+}
