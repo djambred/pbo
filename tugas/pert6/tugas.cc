6bad6c50ab82af3d94baf2ab0d01d499934e7d4f
djambred <djambred@gmail.com>
2021-10-23 11:50:57 +0700

    tambahan tugas pert6

---
+++ tugas/pert6/tugas.cc
@@ -0,0 +1,50 @@
+#include<iostream>
+
+using namespace std;
+
+class Waktu{
+    public:
+        Waktu(int j=0, int m=0, int d=0);
+        void setWaktu( int j, int m, int d);
+        int getJam();
+        int getMenit();
+        int getDetik();
+
+    private:
+        int jam;
+        int menit;
+        int detik;
+};
+
+Waktu::Waktu(int jam, int menit, int detik){
+    setWaktu(jam, menit, detik);
+}
+
+void Waktu::setWaktu(int j, int m, int d){
+    if ((j>=0 && j<24) && (m>=0 && m<=60) && (d>=0 && d<=60)){
+        jam = j;
+        menit = m;
+        detik = d;
+    } else {
+        cout<<"format waktu salah"<<endl;
+    }
+}
+
+int Waktu::getJam(){
+    return jam;
+}
+int Waktu::getMenit(){
+    return menit;
+}
+
+int Waktu::getDetik(){
+    return detik;
+}
+
+
+int main(){    
+    Waktu t;
+    t.setWaktu(20,25,60);
+    cout <<endl<<t.getJam()<<":"<<t.getMenit()<<":"<<t.getDetik()<<endl;
+    return 0;
+}
